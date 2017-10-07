0243078bd128b1af089e184e82eb92bc43e79e7c
Pranay Prakash <pranay.gp@gmail.com>
2017-10-07 17:04:55 -0500

    Gallery View

--- source/components/Gallery/Gallery.jsx
+++ source/components/Gallery/Gallery.jsx
@@ -1,17 +1,87 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {withRouter} from 'react-router-dom'
+import {connect} from 'react-redux';
+import { Image, Card } from 'semantic-ui-react'
+
 import Header from '../Header/Header.jsx';
 
 import styles from './Gallery.scss'
 
 class Gallery extends Component {
-    render() {
-        return(
-            <div className="Gallery">
-                <Header />
-            </div>
-        )
-    }
+
+  renderSongs(songs = []) {
+    return songs.map((song, i) => (
+        <Card key={song.id} onClick={(e) => { this.props.history.push(`/song/${i}`) }}>
+            <Image src={song.album.images[0].url} />
+            <Card.Content>
+            <Card.Header>
+                {song.name}
+            </Card.Header>
+            <Card.Meta>
+                <span>
+                {song.artists.map(a => a.name).join(', ')}
+                </span>
+                <br />
+                <span>
+                {song.album.name}
+                </span>
+            </Card.Meta>
+            </Card.Content>
+        </Card>
+    ))
+  }
+
+  render() {
+    return (
+        <div className='Gallery'>
+            <Header/>
+            {/* <List relaxed='very' animated selection verticalAlign='middle'> */}
+            <Card.Group className="galleryContainer" itemsPerRow={3}>
+                {this.renderSongs(this.props.songs)}
+            </Card.Group>
+            {/* </List> */}
+        </div>
+    );
+  }
 }
 
-export default Gallery
+Gallery.propTypes = {
+  songs: PropTypes.arrayOf(
+    PropTypes.shape({
+      album: PropTypes.shape({
+        artists: PropTypes.array,
+        href: PropTypes.string,
+        id: PropTypes.string,
+        images: PropTypes.arrayOf(
+          PropTypes.shape({
+            height: PropTypes.number,
+            url: PropTypes.string,
+            width: PropTypes.number
+          })
+        ),
+        name: PropTypes.string.isRequired,
+      }),
+      artists: PropTypes.arrayOf(
+        PropTypes.shape({
+          href: PropTypes.string,
+          id: PropTypes.string,
+          name: PropTypes.string.isRequired,
+          uri: PropTypes.string,
+        })
+      ),
+      duration_ms: PropTypes.number,
+      explicit: PropTypes.bool,
+      href: PropTypes.string,
+      id: PropTypes.string,
+      name: PropTypes.string.isRequired,
+      popularity: PropTypes.number,
+      preview_url: PropTypes.string,
+      uri: PropTypes.string
+    })
+  )
+}
+
+export default withRouter(connect(state => ({
+  songs: state.data.results
+}))(Gallery));
--- source/components/Song/Song.jsx
+++ source/components/Song/Song.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
+import {Redirect, withRouter} from 'react-router';
+import { Card, Button, Header, Image } from 'semantic-ui-react'
 import { connect } from 'react-redux';
-import Header from '../Header/Header.jsx';
+import MyHeader from '../Header/Header.jsx';
 
 import styles from './Song.scss'
 
@@ -11,16 +13,43 @@ class Song extends Component {
     const thisPage = Number(this.props.match.params.id);
     const hasPrevPage = thisPage > 0;
     const hasNextPage = thisPage < (this.props.songs.length - 1);
-    console.log(thisPage, hasPrevPage, hasNextPage)
-    return null
+    return (
+      <div>
+        {hasPrevPage ? <Button content='Prev' icon='left arrow' labelPosition='left' onClick={(e) => { this.props.history.push(`/song/${thisPage-1}`) }} /> : null}
+        {hasNextPage ? <Button content='Next' icon='right arrow' labelPosition='right' onClick={(e) => { this.props.history.push(`/song/${thisPage+1}`) }} /> : null}
+      </div>
+    )
   }
 
   render() {
+    const thisPage = Number(this.props.match.params.id);
+    const song = this.props.songs[thisPage];
+    if(!song) {
+      // missing data
+      return <Redirect to='/' />
+    }
     return(
       <div className="Song">
-        <Header search={false}/>
+        <MyHeader search={false}/>
+        <br />
         {this.renderControls()}
-        <h3>ID: {this.props.match.params.id}</h3>
+        <Card>
+          <Image src={song.album.images[0].url} />
+          <Card.Content>
+            <Card.Header>
+              {song.name}
+            </Card.Header>
+            <Card.Meta>
+              <span>
+                {song.artists.map(a => a.name).join(', ')}
+              </span>
+              <br />
+              <span>
+                {song.album.name}
+              </span>
+            </Card.Meta>
+          </Card.Content>
+        </Card>
       </div>
     )
   }
@@ -62,6 +91,6 @@ Song.propTypes = {
   )
 }
 
-export default connect(state => ({
+export default withRouter(connect(state => ({
   songs: state.data.results
-}))(Song)
+}))(Song))
